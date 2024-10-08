17d7f254b13386c67c7b65fd8941dd40cd09a4ab
Darren Zhao <guangyuzhao@hillstonenet.com>
2024-10-08 18:55:01 +0800

    update dependency and add ddm

---
+++ scripts/gendlstudy/ddm/ddm.py
@@ -0,0 +1,426 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 🔥 Diffusion Models
+
+# In this notebook, we'll walk through the steps required to train your own diffusion model on the Oxford flowers dataset
+
+# The code is adapted from the excellent ['Denoising Diffusion Implicit Models' tutorial](https://keras.io/examples/generative/ddim/) created by András Béres available on the Keras website.
+
+import math
+import os
+
+from matplotlib import pyplot as plt
+from gendlstudy.utils import sample_batch, display
+
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras import (
+    datasets,
+    layers,
+    utils,
+    models,
+    optimizers,
+    activations,
+    metrics,
+    losses,
+    callbacks,
+)
+
+
+data_dir = "/home/guangyu/workspace/dataset/pytorch-challange-flower-dataset/dataset"
+base_dir = os.path.dirname(os.path.abspath(__file__))
+checkpoint_dir = f"{base_dir}/checkpoint"
+output_dir = f"{base_dir}/output"
+log_dir = f"{base_dir}/logs"
+model_dir = f"{base_dir}/models"
+
+
+# ## 0. Parameters <a name="parameters"></a>
+
+IMAGE_SIZE = 64
+BATCH_SIZE = 64
+DATASET_REPETITIONS = 5
+LOAD_MODEL = False
+
+NOISE_EMBEDDING_SIZE = 32
+PLOT_DIFFUSION_STEPS = 20
+
+# optimization
+EMA = 0.999
+LEARNING_RATE = 1e-3
+WEIGHT_DECAY = 1e-4
+EPOCHS = 50
+
+
+# ## 1. Prepare the Data
+
+# Load the data
+train_data = utils.image_dataset_from_directory(
+    f"{data_dir}/train",
+    labels=None,
+    image_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=None,
+    shuffle=True,
+    seed=42,
+    interpolation="bilinear",
+)
+
+
+# Preprocess the data
+def preprocess(img):
+    img = tf.cast(img, "float32") / 255.0
+    return img
+
+
+train = train_data.map(lambda x: preprocess(x))
+train = train.repeat(DATASET_REPETITIONS)
+train = train.batch(BATCH_SIZE, drop_remainder=True)
+
+
+# Show some items of clothing from the training set
+train_sample = sample_batch(train)
+display(train_sample)
+
+
+# ### 1.1 Diffusion schedules <a name="diffusion_schedules"></a>
+
+def linear_diffusion_schedule(diffusion_times):
+    min_rate = 0.0001
+    max_rate = 0.02
+    betas = min_rate + diffusion_times * (max_rate - min_rate)
+    alphas = 1 - betas
+    alpha_bars = tf.math.cumprod(alphas)
+    signal_rates = tf.sqrt(alpha_bars)
+    noise_rates = tf.sqrt(1 - alpha_bars)
+    return noise_rates, signal_rates
+
+
+def cosine_diffusion_schedule(diffusion_times):
+    signal_rates = tf.cos(diffusion_times * math.pi / 2)
+    noise_rates = tf.sin(diffusion_times * math.pi / 2)
+    return noise_rates, signal_rates
+
+
+def offset_cosine_diffusion_schedule(diffusion_times):
+    min_signal_rate = 0.02
+    max_signal_rate = 0.95
+    start_angle = tf.acos(max_signal_rate)
+    end_angle = tf.acos(min_signal_rate)
+
+    diffusion_angles = start_angle + diffusion_times * (end_angle - start_angle)
+
+    signal_rates = tf.cos(diffusion_angles)
+    noise_rates = tf.sin(diffusion_angles)
+
+    return noise_rates, signal_rates
+
+# ## 2. Build the model <a name="build"></a>
+
+def sinusoidal_embedding(x):
+    frequencies = tf.exp(
+        tf.linspace(
+            tf.math.log(1.0),
+            tf.math.log(1000.0),
+            NOISE_EMBEDDING_SIZE // 2,
+        )
+    )
+    angular_speeds = 2.0 * math.pi * frequencies
+    embeddings = tf.concat(
+        [tf.sin(angular_speeds * x), tf.cos(angular_speeds * x)], axis=3
+    )
+    return embeddings
+
+
+embedding_list = []
+for y in np.arange(0, 1, 0.01):
+    embedding_list.append(sinusoidal_embedding(np.array([[[[y]]]]))[0][0][0])
+embedding_array = np.array(np.transpose(embedding_list))
+
+
+def ResidualBlock(width):
+    def apply(x):
+        input_width = x.shape[3]
+        if input_width == width:
+            residual = x
+        else:
+            residual = layers.Conv2D(width, kernel_size=1)(x)
+        x = layers.BatchNormalization(center=False, scale=False)(x)
+        x = layers.Conv2D(
+            width, kernel_size=3, padding="same", activation=activations.swish
+        )(x)
+        x = layers.Conv2D(width, kernel_size=3, padding="same")(x)
+        x = layers.Add()([x, residual])
+        return x
+
+    return apply
+
+
+def DownBlock(width, block_depth):
+    def apply(x):
+        x, skips = x
+        for _ in range(block_depth):
+            x = ResidualBlock(width)(x)
+            skips.append(x)
+        # downsampling by half
+        x = layers.AveragePooling2D(pool_size=2)(x)
+        return x
+
+    return apply
+
+
+def UpBlock(width, block_depth):
+    def apply(x):
+        x, skips = x
+        # upsampling by factor of 2
+        x = layers.UpSampling2D(size=2, interpolation="bilinear")(x)
+        for _ in range(block_depth):
+            x = layers.Concatenate()([x, skips.pop()])
+            x = ResidualBlock(width)(x)
+        return x
+
+    return apply
+
+
+# Build the U-Net
+
+noisy_images = layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
+x = layers.Conv2D(32, kernel_size=1)(noisy_images)
+
+noise_variances = layers.Input(shape=(1, 1, 1))
+noise_embedding = layers.Lambda(sinusoidal_embedding)(noise_variances)
+noise_embedding = layers.UpSampling2D(size=IMAGE_SIZE, interpolation="nearest")(
+    noise_embedding
+)
+
+x = layers.Concatenate()([x, noise_embedding])
+
+skips = []
+
+x = DownBlock(32, block_depth=2)([x, skips])
+x = DownBlock(64, block_depth=2)([x, skips])
+x = DownBlock(96, block_depth=2)([x, skips])
+
+x = ResidualBlock(128)(x)
+x = ResidualBlock(128)(x)
+
+x = UpBlock(96, block_depth=2)([x, skips])
+x = UpBlock(64, block_depth=2)([x, skips])
+x = UpBlock(32, block_depth=2)([x, skips])
+
+x = layers.Conv2D(3, kernel_size=1, kernel_initializer="zeros")(x)
+
+unet = models.Model([noisy_images, noise_variances], x, name="unet")
+
+
+class DiffusionModel(models.Model):
+    def __init__(self):
+        super().__init__()
+
+        self.normalizer = layers.Normalization()
+        self.network = unet
+        self.ema_network = models.clone_model(self.network)
+        self.diffusion_schedule = offset_cosine_diffusion_schedule
+
+    def compile(self, **kwargs):
+        super().compile(**kwargs)
+        self.noise_loss_tracker = metrics.Mean(name="n_loss")
+
+    @property
+    def metrics(self):
+        return [self.noise_loss_tracker]
+
+    def denormalize(self, images):
+        images = self.normalizer.mean + images * self.normalizer.variance**0.5
+        return tf.clip_by_value(images, 0.0, 1.0)
+
+    def denoise(self, noisy_images, noise_rates, signal_rates, training):
+        if training:
+            network = self.network
+        else:
+            network = self.ema_network
+        pred_noises = network(
+            [noisy_images, noise_rates**2], training=training
+        )
+        pred_images = (noisy_images - noise_rates * pred_noises) / signal_rates
+
+        return pred_noises, pred_images
+
+    def reverse_diffusion(self, initial_noise, diffusion_steps):
+        num_images = initial_noise.shape[0]
+        step_size = 1.0 / diffusion_steps
+        current_images = initial_noise
+        for step in range(diffusion_steps):
+            diffusion_times = tf.ones((num_images, 1, 1, 1)) - step * step_size
+            noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+            pred_noises, pred_images = self.denoise(
+                current_images, noise_rates, signal_rates, training=False
+            )
+            next_diffusion_times = diffusion_times - step_size
+            next_noise_rates, next_signal_rates = self.diffusion_schedule(
+                next_diffusion_times
+            )
+            current_images = (
+                next_signal_rates * pred_images + next_noise_rates * pred_noises
+            )
+        return pred_images
+
+    def generate(self, num_images, diffusion_steps, initial_noise=None):
+        if initial_noise is None:
+            initial_noise = tf.random.normal(
+                shape=(num_images, IMAGE_SIZE, IMAGE_SIZE, 3)
+            )
+        generated_images = self.reverse_diffusion(
+            initial_noise, diffusion_steps
+        )
+        generated_images = self.denormalize(generated_images)
+        return generated_images
+
+    def train_step(self, images):
+        images = self.normalizer(images, training=True)
+        noises = tf.random.normal(shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
+
+        diffusion_times = tf.random.uniform(
+            shape=(BATCH_SIZE, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+
+        noisy_images = signal_rates * images + noise_rates * noises
+
+        with tf.GradientTape() as tape:
+            # train the network to separate noisy images to their components
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=True
+            )
+
+            noise_loss = self.loss(noises, pred_noises)  # used for training
+
+        gradients = tape.gradient(noise_loss, self.network.trainable_weights)
+        self.optimizer.apply_gradients(
+            zip(gradients, self.network.trainable_weights)
+        )
+
+        self.noise_loss_tracker.update_state(noise_loss)
+
+        for weight, ema_weight in zip(
+            self.network.weights, self.ema_network.weights
+        ):
+            ema_weight.assign(EMA * ema_weight + (1 - EMA) * weight)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, images):
+        images = self.normalizer(images, training=False)
+        noises = tf.random.normal(shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
+        diffusion_times = tf.random.uniform(
+            shape=(BATCH_SIZE, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+        noisy_images = signal_rates * images + noise_rates * noises
+        pred_noises, pred_images = self.denoise(
+            noisy_images, noise_rates, signal_rates, training=False
+        )
+        noise_loss = self.loss(noises, pred_noises)
+        self.noise_loss_tracker.update_state(noise_loss)
+
+        return {m.name: m.result() for m in self.metrics}
+
+
+ddm = DiffusionModel()
+ddm.normalizer.adapt(train)
+
+
+if LOAD_MODEL:
+    ddm.built = True
+    ddm.load_weights(f"{checkpoint_dir}/checkpoint.ckpt")
+
+
+# ## 3.Train the model <a name="train"></a>
+
+ddm.compile(
+    optimizer=optimizers.experimental.AdamW(
+        learning_rate=LEARNING_RATE, weight_decay=WEIGHT_DECAY
+    ),
+    loss=losses.mean_absolute_error,
+)
+
+
+# run training and plot generated images periodically
+model_checkpoint_callback = callbacks.ModelCheckpoint(
+    filepath=f"{checkpoint_dir}/checkpoint/checkpoint.ckpt",
+    save_weights_only=True,
+    save_freq="epoch",
+    verbose=0,
+)
+
+tensorboard_callback = callbacks.TensorBoard(log_dir=log_dir)
+
+
+class ImageGenerator(callbacks.Callback):
+    def __init__(self, num_img):
+        self.num_img = num_img
+
+    def on_epoch_end(self, epoch, logs=None):
+        generated_images = self.model.generate(
+            num_images=self.num_img,
+            diffusion_steps=PLOT_DIFFUSION_STEPS,
+        ).numpy()
+        display(
+            generated_images,
+            save_to=f"{output_dir}/generated_img_%03d.png" % (epoch),
+        )
+
+
+image_generator_callback = ImageGenerator(num_img=10)
+
+ddm.fit(
+    train,
+    epochs=EPOCHS,
+    callbacks=[
+        model_checkpoint_callback,
+        tensorboard_callback,
+        image_generator_callback,
+    ],
+)
+
+
+# ## 4. Inference <a name="inference"></a>
+
+# Generate some novel images of flowers
+generated_images = ddm.generate(num_images=10, diffusion_steps=20).numpy()
+display(generated_images)
+
+
+# View improvement over greater number of diffusion steps
+for diffusion_steps in list(np.arange(1, 6, 1)) + [20] + [100]:
+    tf.random.set_seed(42)
+    generated_images = ddm.generate(
+        num_images=10,
+        diffusion_steps=diffusion_steps,
+    ).numpy()
+    display(generated_images)
+
+
+# Interpolation between two points in the latent space
+tf.random.set_seed(100)
+
+
+def spherical_interpolation(a, b, t):
+    return np.sin(t * math.pi / 2) * a + np.cos(t * math.pi / 2) * b
+
+
+for i in range(5):
+    a = tf.random.normal(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
+    b = tf.random.normal(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
+    initial_noise = np.array(
+        [spherical_interpolation(a, b, t) for t in np.arange(0, 1.1, 0.1)]
+    )
+    generated_images = ddm.generate(
+        num_images=2, diffusion_steps=20, initial_noise=initial_noise
+    ).numpy()
+    display(generated_images, n=11)
+
+
+
+
